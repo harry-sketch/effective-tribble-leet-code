c957cccf9851c91db5379f3e6e958173cda0f585
harry-sketch <harshbisht90@gmail.com>
2023-05-31 12:25:48 +0530

    add: table

--- src/components/Problems/ProblemsTable.tsx
+++ src/components/Problems/ProblemsTable.tsx
@@ -1,24 +1,58 @@
 import React from "react";
+import { problems } from "@/Data/Data";
+import { cw } from "@/utils/helpers";
 
 const ProblemsTable = () => {
-  const headerData = [{}];
+  const headerData: Array<string> = [
+    "Status",
+    "Title",
+    "Difficulty",
+    "Category",
+    "Solution",
+  ];
 
   return (
-    <table className="max-w-6xl mx-auto mt-6 w-full">
+    <table className="max-w-4xl mx-auto mt-6 w-full">
       <thead>
-        <tr className="text-gray-600 uppercase text-sm w-full">
-          <th>Status</th>
-          <th>Title</th>
-          <th>Difficulty</th>
-          <th>Category</th>
-          <th>Solution</th>
+        <tr className="text-gray-600 uppercase text-sm w-full border-b border-gray-600">
+          {headerData.map((data, i) => (
+            <th key={i} className="pb-3 text-start">
+              {data}
+            </th>
+          ))}
         </tr>
       </thead>
 
       <tbody>
-        <tr>
-          <td></td>
-        </tr>
+        {problems.map(
+          ({ category, difficulty, id, order, title, videoId }, idx) => (
+            <tr
+              className={cw(
+                "text-slate-50 text-sm font-normal h-14",
+                idx % 2 === 0 ? "bg-transparent" : "bg-[#282828]"
+              )}
+              key={id}
+            >
+              <td />
+              <td>
+                {order}. {title}
+              </td>
+              <td
+                className={cw(
+                  difficulty === "Easy"
+                    ? "text-green-500"
+                    : difficulty === "Medium"
+                    ? "text-yellow-500"
+                    : "text-red-500"
+                )}
+              >
+                {difficulty}
+              </td>
+              <td>{category}</td>
+              <td>{videoId ? "" : "Coming Soon"}</td>
+            </tr>
+          )
+        )}
       </tbody>
     </table>
   );
